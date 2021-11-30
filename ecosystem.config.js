645401947c706eaeda6aa8ae2b434ac8648b627f
Antonin <antonin.raymondaud@gmail.com>
2021-11-30 23:46:08 +0100

    pre-deploy-local

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       host:'192.168.1.152',
       ref  : 'origin/main',
       repo : 'git@github.com:FIntLyncix/collaps-api.git',
-      path : '/home/sergio/Documents/testpm2',
+      path : '/home/sergio/Documents/collaps-api',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
