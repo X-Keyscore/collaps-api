89d3237dddb50f73f03c2d3ba348cb4654a429fb
Antonin <antonin.raymondaud@gmail.com>
2021-11-30 23:34:10 +0100

    pre-deploy-local

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       user : 'sergio',
       host:'192.168.1.152',
       ref  : 'origin/main',
-      repo : 'git@github.com:FIntLyncix/fint-tchat-api.git',
+      repo : 'git@github.com:FIntLyncix/collaps-api.git',
       path : '/home/sergio/Documents/testpm2',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
