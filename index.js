87a284dbfe2ed6921133cb6ca3e377b09a37cd78
Antonin <antonin.raymondaud@gmail.com>
2021-12-02 20:07:12 +0100

    pre-deploy-local

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ cors = require('cors');
 
 const io = require('socket.io')(http, {
 	cors: {
-		origin: "https://optimistic-bohr-e15b9b.netlify.app" //http://localhost:8000
+		origin: "https://collaps.netlify.app/" //http://localhost:8000
 	}
 });
 /*
